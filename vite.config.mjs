7c905953730a12bf8fbc2261dc17bf6fc9224f00
benharmonics <benjamin.jiron+git@gmail.com>
2025-09-17 13:15:31 -0600

    fix vite build - build target set to esnext

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
+  build: { target: "esnext" },
   root: ".", // root is the current folder with index.html
   plugins: [tailwindcss(), wasm()],
 });
