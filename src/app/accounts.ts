260792d2a7ac88621466084c296c9c18e67eac07
benharmonics <benjamin.jiron+git@gmail.com>
2025-09-17 13:15:31 -0600

    fix tailwind

--- src/app/accounts.ts
+++ src/app/accounts.ts
@@ -1,30 +1,40 @@
 import Wallet from "../wallet";
-//import { formatUnits } from "ethers";
+import { formatEther } from "ethers";
 import { encryptVault, decryptVault, hasVault } from "./vault";
 
 type WalletState = {
   mnemonic?: string | null;
+  wallet?: Wallet | null;
   hasVault: boolean;
+  addressIndex: number;
 };
 
-const state: WalletState = { hasVault: hasVault() };
+const state: WalletState = { hasVault: hasVault(), addressIndex: 0 };
 
-export function enterCredentials() {
+export function walletPage() {
   return `
-    <h1 class="text-4xl">Wallet</h1>
-    <div class="flex flex-col space-y-2">
+    <h1 class="text-4xl p-5 min-w-128 text-white font-semibold bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded">Wallet</h1>
+    <div class="flex flex-col space-y-5">
       <div id="vault-status"></div>
       <div class="flex space-x-5">
-        <button id="unlock" class="text-white font-semibold rounded-3xl p-5 bg-blue-600 hover:bg-blue-700">🔓 Unlock Vault</button>
-        <button id="save" class="text-white font-semibold rounded-3xl p-5 bg-gray-800 hover:bg-gray-900">🔑 ${state.hasVault ? "Enter New" : "Save"} Mnemonic</button>
+        <button id="unlock" class="text-white font-semibold rounded-3xl px-5 py-2 bg-blue-700 hover:bg-blue-800">🔓 Unlock Vault</button>
+        <button id="save" class="text-white font-semibold rounded-3xl px-5 py-2 bg-gray-800 hover:bg-gray-900">🔑 ${state.hasVault ? "Enter New" : "Save"} Mnemonic</button>
       </div>
-      <pre id="output" class="py-2 px-5 bg-rose-500 hidden"></pre>
-      <div id="wallet-buttons" class="flex space-x-5 items-center hidden">
-        <button id="send" class="rounded-4xl">Send</button>
+      <pre id="output" class="py-2 px-5 bg-neutral-300 text-gray-900 rounded-3xl hidden"></pre>
+      <div id="wallet-buttons" class="hidden space-y-5">
         <div class="flex space-x-5 items-center">
-          <span>Balance:</span>
-          <span id="balance"></span>
+          <span class="text-lg">Balance:</span>
+          <span id="balance" class="text-xl font-semibold"></span>
         </div>
+        <div class="flex space-x-2 items-center">
+          <div class="flex space-x-1 items-center">
+            <button id="decrement-address-index" class="text-xs bg-blue-700 hover:bg-blue-800 p-1 rounded">Prev</button>
+            <button id="increment-address-index" class="text-xs bg-blue-700 hover:bg-blue-800 p-1 rounded">Next</button>
+          </div>
+          <span id="address-index" class="text-sm">Address ${state.addressIndex}:</span>
+          <span id="address" class="text-xs"></span>
+        </div>
+        <button id="send" class="bg-linear-to-bl from-violet-500 to-fuchsia-500 py-2 px-4 rounded-xl text-xl text-white text-shadow font-semibold">Send</button>
       </div>
     </div>
   `;
@@ -55,6 +65,7 @@ export function bindWalletEvents() {
       state.mnemonic = result;
       showHelpText();
       getBalance();
+      getAddress();
       output!.textContent = `🔓 Vault unlocked!`;
       output!.classList.remove("hidden");
       walletButtons!.classList.remove("hidden");
@@ -63,20 +74,49 @@ export function bindWalletEvents() {
     }
   };
 
+  document.getElementById("increment-address-index")!.onclick = async () => {
+    state.addressIndex++;
+    await setAddressIndex(state.addressIndex);
+  }
+
+  document.getElementById("decrement-address-index")!.onclick = async () => {
+    if (state.addressIndex <= 0) return;
+    state.addressIndex--;
+    await setAddressIndex(state.addressIndex);
+  }
+
   showHelpText();
 }
 
-async function getBalance() {
+async function getBalance(wallet?: Wallet) {
   if (!state.mnemonic) {
     return "Unavailable - please enter a mnemonic.";
   }
-  const wallet = new Wallet(state.mnemonic);
   const balance = document.getElementById("balance");
-  balance!.textContent = await wallet.balance().then((bal) => bal.toString());
+  balance!.textContent = "Loading...";
+  wallet ??= new Wallet(state.mnemonic);
+  const balanceWei = await wallet.balance(state.addressIndex);
+  balance!.textContent = `${formatEther(balanceWei)} ETH`;
+}
+
+async function getAddress(wallet?: Wallet) {
+  if (!state.mnemonic) {
+    return "Unavailable - please enter a mnemonic.";
+  }
+  wallet ??= new Wallet(state.mnemonic);
+  document.getElementById("address")!.textContent = await wallet.address(state.addressIndex);
+}
+
+async function setAddressIndex(addressIndex: number, wallet?: Wallet) {
+  if (!state.mnemonic) {
+    return "Unavailable - please enter a mnemonic.";
+  }
+  document.getElementById("address-index")!.textContent = `Address: ${addressIndex}`;
+  getBalance(wallet);
+  getAddress(wallet);
 }
 
 function showHelpText() {
-  console.log(`State: ${JSON.stringify(state, null, 2)}`);
   const vaultStatus = document.getElementById("vault-status");
   if (state.hasVault && !state.mnemonic) {
     vaultStatus!.textContent = `Enter your password to unlock your wallet`;
